9ab41d2d852d1a606f6337519a73459b4af5b4df
Bookworm2024 <infohubnetworks@gmail.com>
2024-07-25 10:29:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,44 +13,44 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "21145186"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "daa53f4216112ad22b8a8f6299936a46")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002160865517"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6011680723"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://infohubstore06:Mf8U56mbjVqeN3Gm@fileshare.ir0tgdu.mongodb.net/?retryWrites=true&w=majority&appName=fileshare")
+DB_NAME = os.environ.get("DATABASE_NAME", "fileshare")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "0"))
-FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "0"))
-FORCE_SUB_CHANNEL_3 = int(os.environ.get("FORCE_SUB_CHANNEL_3", "0"))
-FORCE_SUB_CHANNEL_4 = int(os.environ.get("FORCE_SUB_CHANNEL_4", "0"))
+FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002123546604"))
+FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "-1002170049626"))
+FORCE_SUB_CHANNEL_3 = int(os.environ.get("FORCE_SUB_CHANNEL_3", "-1001993667849"))
+FORCE_SUB_CHANNEL_4 = int(os.environ.get("FORCE_SUB_CHANNEL_4", "-1002182645748"))
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "ʜᴇʟʟᴏ ᴛʜᴇʀᴇ!! {mention} 👋\n\nɪ ᴀᴍ ᴀ ꜰɪʟᴇ ꜱʜᴀʀᴇ ʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ᴀɴᴅ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ ɪɴꜰᴏʜᴜʙ ɴᴇᴛᴡᴏʀᴋꜱ.🍂💫\n\n⚠️ᴅᴏ ɴᴏᴛ ꜱᴇɴᴅ ᴍᴇ ᴅɪʀᴇᴄᴛ ᴍᴇꜱꜱᴀɢᴇꜱ ꜰᴏʀ ɪ ᴀᴍ ɴᴏᴛ ᴅᴇꜱɪɢɴᴇᴅ ᴛᴏ ʀᴇᴄᴇɪᴠᴇ ᴛʜᴇᴍ!!⚠️")
 try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
+    ADMINS=[6011680723]
+    for x in (os.environ.get("ADMINS", "6011680723 1524169222 2007123863 7000915053 6792991359").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "𝐇𝐞𝐥𝐥𝐨, {mention}\n\n<b>𝐔𝐦𝐦, 𝐬𝐨𝐫𝐫𝐲 𝐭𝐨 𝐢𝐧𝐭𝐞𝐫𝐫𝐮𝐩𝐭 𝐛𝐮𝐭 𝐩𝐥𝐞𝐚𝐬𝐞 𝐣𝐨𝐢𝐧 𝐭𝐡𝐞 𝐟𝐨𝐥𝐥𝐨𝐰𝐢𝐧𝐠 𝐜𝐡𝐚𝐧𝐧𝐞𝐥𝐬 𝐛𝐞𝐟𝐨𝐫𝐞 𝐈 𝐬𝐞𝐧𝐝 𝐲𝐨𝐮 𝐭𝐡𝐞 𝐟𝐢𝐥𝐞(𝐬).</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -62,10 +62,10 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "ᴡʜʏ ɪɴ ᴛʜᴇ ᴡᴏʀʟᴅ ᴡɪʟʟ ʏᴏᴜ ꜱᴇɴᴅ ᴍᴇ ᴍᴇꜱꜱᴀɢᴇꜱ?? ɪ'ᴍ ᴊᴜꜱᴛ ᴀ ꜰɪʟᴇ ꜱʜᴀʀᴇ ʙᴏᴛ, ʙᴜᴅᴅʏ.\n\nᴜꜱᴇ @infohubrequest_robot ᴛᴏ ᴘʟᴀᴄᴇ ᴍᴏʀᴇ ʀᴇQᴜᴇꜱᴛꜱ."
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(6011680723)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
